5880e50b9ea40818d7fe09dd8cc2dc767ed381b3
illusion-megrim <3381603476@qq.com>
2020-04-23 12:50:33 +0800

    test

---
+++ src/com/neusoft/collection/teach/Hello.java
@@ -0,0 +1,11 @@
+package com.neusoft.collection.teach;
+
+/**
+ * @Author: wwb
+ * @Date: 2020/4/23 12:40
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
