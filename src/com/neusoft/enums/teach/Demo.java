b409aa1f99c9910e0c80bbd0242407fd6aa8c785
illusion-megrim <3381603476@qq.com>
2020-04-23 14:47:04 +0800

    测试

---
+++ src/com/neusoft/enums/teach/Demo.java
@@ -0,0 +1,12 @@
+package com.neusoft.enums.teach;
+
+/**
+ * @Author: wwb
+ * @Date: 2020/4/23 14:42
+ */
+public class Demo {
+    public static void main(String args[]){
+        System.out.println("À²À²À²");
+
+    }
+}
